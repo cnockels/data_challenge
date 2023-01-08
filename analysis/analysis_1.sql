464c7b95c9c6280e4e3ec3fdae984a1ce07ef0f1
cnockels <thenockels@gmail.com>
2023-01-08 14:07:11 +0000

    analysis_1

---
+++ analysis/analysis_1.sql
@@ -0,0 +1,49 @@
+with 
+    Orders as (
+        select * from {{ source('source_tables', 'orders') }}, 
+        unnest(line_items) as line_items 
+        qualify updated_at = max(updated_at) over (partition by _id)
+    ),
+    Products as (
+        select * from {{ source('source_tables', 'products') }} 
+        qualify updated_at = max(updated_at) over (partition by _id)
+    ),  
+    
+    qryProducts as 
+    (
+        select 
+            Products.* except(variants),
+            unnested.variant_id as product_variant_id,
+            unnested.title as variant_title,
+            unnested.sku as product_sku,
+            unnested.created_at as variant_created_at,
+            unnested.updated_at as variant_updated_at,
+            unnested.option1 as product_style,
+            unnested.option2 as product_size
+        from 
+            Products cross join
+            unnest(variants) as unnested
+      qualify unnested.updated_at = max(unnested.updated_at) over (partition by unnested.variant_id)
+    ),  
+
+    qryJoin as 
+    (
+        select 
+    		Orders._id, 
+    		qryProducts.category,
+    		qryProducts.product_size,
+        from 
+    		Orders left join
+    		qryProducts on
+    			Orders.variant_id = qryProducts.product_variant_id
+    ),  
+
+    qryKingSheets as 
+    (
+        select
+            concat(round(sum(case when category = 'Sheet Sets' and product_size = 'King' then 1.0 end) / count(*) * 100, 2), ' %') as king_sheet_order_percent
+        from
+            qryJoin
+    )
+
+select * from qryKingSheets
